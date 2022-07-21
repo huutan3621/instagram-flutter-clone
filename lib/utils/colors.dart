b7d98dea1c3c1ca6cf49e9e42d7ef3c8d40a8c8c
huutan3621 <huutan3621@gmail.com>
2022-07-22 00:58:46 +0700

    add responsive and dimensions

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//colors used in the app, theme of the app
 const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
